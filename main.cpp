a0edd3d4802f2caabc41ebe21c163d3cabbbbdba
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-01-10 20:06:20 +0400

    fixed main window construction

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include "controller.h"
 #include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Controller controller;
+
+    MainWindow *w = new MainWindow(&controller);
+    w->show();
 
     return a.exec();
 }
